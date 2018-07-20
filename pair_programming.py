0cc4dd6c7bd2d443d347e7e947400335db4972ae
Michelle Kuchera <mikuchera@davidson.edu>
2018-07-20 09:50:40 -0500

    pair programming script easier to use in a modular way.

--- pair_programming.py
+++ pair_programming.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
+file: pair_programming.py
 
-pair programming code for in-class activities.
+Pair programming code for in-class activities.
+Randomly assigns partners to tables and does the pair-programming timing.
 
-
-
-@author: M.P. Kuchera
-@date: 1.29.2018
-@brief: randomly assigns partners to tables and does the pair-programming
-        timing.
+author: M.P. Kuchera
+date: 29 Jan 2018
+edited: 16 July 2018
 """
 
 
@@ -19,32 +17,32 @@ import os
 import sys
 
 # global variables
-students = ["Michelle Kuchera", "Jack Taylor", "Anthony Kuchera", "Alex Karbo", "Maggie Perry", \
-            "Matt Perry", "Zuri Brandt", "Mark Newman", "Paul Tipler"]
+students = ["Michelle Kuchera", "Jack Taylor", "Anthony Kuchera", "Maggie Perry", \
+            "Matt Perry", "Zuri Brandt", "Mark Newman", "Paul Tipler", "Sam Tabor", \
+            "Ernest Hemmingway"]
 
 
-def main():
-    
+def partner_assignments():
     print("there are", len(students), "students in the class. They are:")
     for student in students:
         print(student, end=",   ")
-    
+
     students_per_group = 2
-    
+
     num_groups = len(students) / students_per_group
-      
+
     print("\n\nThere are",num_groups, "groups in the class.")
-    
+
     np.random.shuffle(students)
-    
+
     for i in range(0,int(num_groups)):
         print("\nTable ",i+1,": ",students[i*students_per_group],"and" ,students[i*students_per_group+1],end=" ")
       #  print(students[i*students_per_group],"and " ,students[i*students_per_group+1] )
-    
+
     if(len(students)%students_per_group != 0):
         print("and" ,students[-1])
 
-        
+def coding_time():
     input("\n\nPress `Enter` when ready to start pair programming:")
     inp_min = input('How much time do the students have in minutes? ')
     minutes = float(inp_min)
@@ -53,17 +51,26 @@ def main():
     one_turn = float(inp_turn)*60
     print("\nStart coding!")
     os.system('say "Start Coding!"')
-    
+
     for t in range(0,int(tot_time/one_turn)):
         time.sleep(one_turn)
         print("Time to switch roles!")
         sys.stdout.flush()
         os.system('say "Switch roles!"')
-    
+
     time.sleep(one_turn)
     print("Time is up")
     sys.stdout.flush()
     os.system('say Time is up. ')
+
+
+# main function
+def main():
+    # set partners and tables
+    partner_assignments()
+    #
+    coding_time()
+
+# run main() function iff we are executing THIS file.
 if __name__ == "__main__":
     main()
-    
